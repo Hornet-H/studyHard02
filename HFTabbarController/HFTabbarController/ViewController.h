3daa5e2c8371388eb88817081d160cc3fda5708e
黄来峰 <huangfeng8912@163.com>
2016-10-09 10:53:37 +0800

    提交工程文件

---
+++ HFTabbarController/HFTabbarController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HFTabbarController
+//
+//  Created by 黄来峰 on 16/10/9.
+//  Copyright © 2016年 dahuangfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HFTabbarController/HFTabbarController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HFTabbarController
+//
+//  Created by 黄来峰 on 16/10/9.
+//  Copyright © 2016年 dahuangfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
